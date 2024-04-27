d372e716cff90534ed18957db9d6ddc30181794e
yasin-arft <yasinarftsocial@gmail.com>
2024-04-27 11:29:42 +0600

    mongodb connection setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jzumutc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 // middlewares
 app.use(cors());
@@ -11,6 +23,23 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Jutood Crafts sever is running');
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Jutood Crafts sever is running on ${port} PORT`);
